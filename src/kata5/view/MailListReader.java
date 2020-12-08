582bbe086a6863d52cf69f7eb2d67ca56888ccce
j03Vincent <72548097+j03Vincent@users.noreply.github.com>
2020-12-08 13:52:20 +0000

    Update MailListReader.java

--- src/kata5/view/MailListReader.java
+++ src/kata5/view/MailListReader.java
@@ -1,10 +1,5 @@
 package kata5.view;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
